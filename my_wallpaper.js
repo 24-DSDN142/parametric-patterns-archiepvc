30202d18b9b6c40ea54b8b151088581d236687fd
archiepvc <zoecolliedog@gmail.com>
2024-07-25 14:25:54 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,6 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let triangleSidesX = 20;
+let triangleSidesY = 100;
 
 
 function setup_wallpaper(pWallpaper) {
@@ -10,7 +10,7 @@ function setup_wallpaper(pWallpaper) {
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_height = 150;
   pWallpaper.grid_settings.row_offset  = 50;
 }
 
@@ -19,5 +19,54 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+
+  DrawWing();
+scale(-1,1);
+translate(-200,0);
+  DrawWing();
+
+
+translate(100,20);
+triangle(0, 50, triangleSidesX, triangleSidesY, -triangleSidesX, triangleSidesY);
+scale(1,-1);
+
+translate(0,-90);
+triangle(0, 50, triangleSidesX, triangleSidesY, -triangleSidesX, triangleSidesY);
 }
+
+ function DrawWing(){
+  strokeWeight(1);
+  stroke(0);
+  fill(0)
+  
+  beginShape();
+  vertex(5, 20,);
+  vertex(10, 45);
+  vertex(68, 45);
+  vertex(66, 35)
+
+ endShape(CLOSE);
+
+ beginShape();
+ vertex(13, 62,);
+ vertex(23, 88,);
+ vertex(73, 65,);
+ vertex(70, 55,)
+
+endShape(CLOSE);
+
+beginShape();
+vertex(35, 105,);
+vertex(65, 120,);
+vertex(82, 85);
+vertex(77, 77,)
+
+endShape(CLOSE);
+
+beginShape();
+vertex(66, 35,);
+vertex(80, 39,);
+vertex(90, 73);
+vertex(80, 90,)
+endShape(CLOSE);
+ }
