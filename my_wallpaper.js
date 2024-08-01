0b8d2e2084a1341e64a57e62cc24f8d63085650f
archiepvc <zoecolliedog@gmail.com>
2024-08-01 12:33:51 +1200

    new stuff

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,30 +1,78 @@
 //your parameter variables go here!
-let triangleSidesX = 20;
-let triangleSidesY = 100;
-
+let triangleSidesX = 20; 
+let triangleSidesY = 89; // 100 or 90
+let triangleColour = 1;
+let EllipseBG = true
+let eyeHeight = 30 // closes and opens eye
+let eyeX = 15
+let drawIrisEye = 2
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.resolution(NINE_LANDSCAPE);
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 150;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+background(235, 215, 199)
+
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-  DrawWing();
-scale(-1,1);
-translate(-200,0);
-  DrawWing();
+//drawEye();
 
+//drawIris();
+if (drawIrisEye > 1){
+  fill(214, 84, 58)
+  strokeWeight(1)
+  stroke(161, 25, 22)
+  ellipse(100,45,50,eyeHeight)
+ 
+  fill(235, 102, 45)
+  strokeWeight(1)
+  ellipse(100,45,20,eyeHeight)
+
+}
+
+if(triangleSidesY == 90){
+  EllipseBG = true
+}
+else
+{EllipseBG = false}
+
+if(EllipseBG == true){
+ strokeWeight(2)
+ noStroke()
+  fill(235, 102, 45)
+  ellipse(100, 115, 100, 100);
+
+}
+
+
+
+push();
+translate(0, 45);
+
+  DrawWing();  // this creates the wing on the left
+scale(-1,1); // mirrors the wing
+translate(-200,0); // translates it
+  DrawWing(); // this creates the wing on the right
+
+if(triangleColour == 1){
+  fill(214, 84, 58)
+}
+else if(triangleColour == 2){
+  //fill 2
+}
+else{
+  fill(255, 0, 0);
+}
 
 translate(100,20);
 triangle(0, 50, triangleSidesX, triangleSidesY, -triangleSidesX, triangleSidesY);
@@ -32,12 +80,14 @@ scale(1,-1);
 
 translate(0,-90);
 triangle(0, 50, triangleSidesX, triangleSidesY, -triangleSidesX, triangleSidesY);
+pop();
+
 }
 
- function DrawWing(){
+ function DrawWing(){ // this is the function for a wing
   strokeWeight(1);
-  stroke(0);
-  fill(0)
+  stroke(77, 15, 10);
+  fill(201, 22, 22)
   
   beginShape();
   vertex(5, 20,);
@@ -70,3 +120,17 @@ vertex(90, 73);
 vertex(80, 90,)
 endShape(CLOSE);
  }
+
+//function drawIris (x,y){
+  //fill(235, 102, 45)
+  //strokeWeight(1)
+  //ellipse(100,45,20,eyeHeight)
+//}
+
+//function drawEye (x,y){ // this is the function for the eye
+  //fill(214, 84, 58)
+//strokeWeight(1)
+//stroke(161, 25, 22)
+//ellipse(100,45,50,eyeHeight)
+
+//}
